61a1bf471331a876f03d23864be19e75c3da52bc
moskalyk <morgan.moskalyk@protonmail.ch>
2024-10-07 08:56:08 -0400

    cleaned up comments

--- examples/ts-webrtc/api/index.ts
+++ examples/ts-webrtc/api/index.ts
@@ -20,19 +20,13 @@ export default class RelaySystem {
 
         this.websocketRef.onmessage = (message: any) => {
             const data = JSON.parse(message.data);
-            console.log(data)
             this.handleSignalingMessage(data);
       
             if(data.peers&&data.peers.length>=2){
+              console.log('RELAY_CONNECT')
               this.connectToRelayAndDestination(data.peers.slice(1,data.peers.length),Number(data.message)+1)
             }
         };
-
-        // this.websocketRef.onmessage = (message: any) => {
-        //   const data = JSON.parse(message.data);
-        //   console.log('WebSocket received:', data);
-        //   this.handleSignalingMessage(data);
-        // };
     
         this.websocketRef.onclose = () => {
           console.log('Disconnected from signaling server');
@@ -99,7 +93,6 @@ export default class RelaySystem {
 
     private setupPeerConnection(remoteClientId: any, peerConnection: any) {
         this.peerConnections[remoteClientId] = peerConnection;
-        // this.setPeerConnections=...this.peerConnections
         var self  = this;
 
         // Handle ICE candidates
@@ -116,45 +109,33 @@ export default class RelaySystem {
         peerConnection.ondatachannel = (event: any) => {
           const receivedChannel = event.channel;
           console.log('Data channel opened on receiver side for client:', remoteClientId);
-          
 
           receivedChannel.onmessage = (e: any) => {
-            let { initiator, message, peers, origin } = JSON.parse(e.data);
-
-            // if(!initiator){
+            let { message, peers, origin } = JSON.parse(e.data);
 
             console.log('origin', origin)
-            // console.log()
             console.log('Received:', message, 'for peers', peers);
 
-            // if(message < peers.length) {
                 let interval = setTimeout(() => {
-            //         console.log('RUNNING')
-            //         console.log('Received:', message, 'for peers', peers, 'origin', origin);
             if (!self.hasMessageBeenProcessed(origin, message)) {
                 self.markMessageAsProcessed(origin, message);
-            message += 1;  // Increment the message
+                message += 1;  // Increment the message
 
-                    if (peers[message]) {
-            //             // Safeguard to ensure forwarding only happens once
-                        
-
-                            self.triggerEvent('forward', { message, peers: peers.slice(1,peers.length) }, (payload: any) => {
-                                clearInterval(interval);
-                                self.forwardMessage(peers[message], payload);
-                            });
-                        }
-                    } else {
+                if (peers[message]) {
+                    self.triggerEvent('forward', { message, peers: peers.slice(1,peers.length) }, (payload: any) => {
                         clearInterval(interval);
-                        console.log("Final destination reached:", message);
-                    }
-                }, 100);
-            // }
+                        self.forwardMessage(peers[message], payload);
+                    });
+                }
+            } else {
+                clearInterval(interval);
+                console.log("Final destination reached:", message);
+            }
+            }, 100);
         }
     }
     
         this.dataChannels[remoteClientId] = peerConnection.createDataChannel('messageChannel');
-        // setDataChannels({ ...dataChannels });
     
         // Handle the case where we are the initiator
         this.dataChannels[remoteClientId].onopen = () => {
@@ -165,9 +146,6 @@ export default class RelaySystem {
     public connectToRelayAndDestination (peers: any, counter: number) {
         var self = this
         peers.map(async (peerId: any, index: any) => {
-          console.log('setting up relay')
-          console.log(peers)
-          console.log(counter)
           const peerConnection = new RTCPeerConnection();
           self.setupPeerConnection(peerId, peerConnection);
       
@@ -184,18 +162,8 @@ export default class RelaySystem {
           // Send initial message from the origin (index 0)
           if (index === 0) {
             self.dataChannels[peerId].onopen = () => {
-                // const origin = Date.now()
-                // console.log(origin)
                 const origin = window.location.pathname.slice(1,window.location.pathname.length);
-
-                // Log the path to the console
-                console.log(origin);
-                console.log(peerId)
-                // const message = counter;
                 self.dataChannels[peerId].send(JSON.stringify({ initiator: true, origin: origin, message: counter, peers: peers }));
-                // self.triggerEvent('forward', { message, peers: peers }, (payload: any) => {
-                //     self.forwardMessage(peerId, payload);
-                // });
             };
           }
         });
